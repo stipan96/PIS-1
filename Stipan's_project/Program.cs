10f522d4ece59b9f70a096ef1633e2ef69847ecb
stipan96 <stipan96@gmail.com>
2019-03-26 16:03:53 +0100

    First commit

---
+++ Stipan's_project/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stipan_s_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
